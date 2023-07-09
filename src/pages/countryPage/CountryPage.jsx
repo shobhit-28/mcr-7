b93564a8470006aa4b2813f7ff62d5f726488d2b
shobhit-28 <shobhitraj34@gmail.com>
2023-07-09 21:55:05 +0530

    commit 3

--- src/pages/countryPage/CountryPage.jsx
+++ src/pages/countryPage/CountryPage.jsx
@@ -13,7 +13,7 @@ export const CountryPage = () => {
         <div className="page">
             <p className="text-3xl font-medium text-center my-5">Top Countries in {data.continents.find(({ id }) => id === Number(countryID)).name} for your next holiday</p>
             <div className="flex justify-evenly my-28">
-                {countryData.map(({ id, image, name }) => <div key={id} className="w-1/4" onClick={() => navigate(`/destination/${id}`)} ><Card image={image} name={name} /></div>)}
+                {countryData.map(({ id, image, name }) => <div key={id} className="w-1/4" onClick={() => navigate(`/destination/${id}-${name}-${countryID}`)} ><Card image={image} name={name} /></div>)}
             </div>
         </div>
     )
--- src/pages/destinationPage/DestinationPage.jsx
+++ src/pages/destinationPage/DestinationPage.jsx
@@ -1,19 +1,22 @@
 import { useParams } from "react-router-dom"
 import { data } from "../../data/data"
+import { Card } from "../../components/card"
 // import { Card } from "../../components/card"
 
 export const DestinationPage = () => {
     const { destinationID } = useParams()
 
-    const countryData = data.continents.find(({ id }) => id === Number(destinationID)).countries
-    console.log(countryData)
+    const destId = destinationID.split('-')[0]
+    const countryID = destinationID.split('-')[2]
+
+    const destinationsData = data.continents.find(({ id }) => id === Number(countryID)).countries.find(({id}) => id === Number(destId)).destinations
 
     return (
         <div className="page">
-            {/* <p className="text-3xl font-medium text-center my-5">Top Countries in {data.continents.find(({ id }) => id === Number(countryID)).name} for your next holiday</p>
-            <div className="flex justify-evenly my-28">
-                {countryData.map(({ id, image, name }) => <div key={id} className="w-1/4" ><Card image={image} name={name} /></div>)}
-            </div> */}
+            <p className="text-3xl font-medium text-center my-5">Top Countries in {data.continents.find(({ id }) => id === Number(countryID)).countries.find(({id}) => id === Number(destId)).name} for your next holiday</p>
+            <div className="flex justify-evenly my-28 flex-wrap">
+                {destinationsData.map(({ id, image, name }) => <div key={id} className="w-1/5" ><Card image={image} name={name} /></div>)}
+            </div>
         </div>
     )
 }
